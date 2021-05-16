e7f511c986cd7fe3e27d6e541e5537143d8f8d9d
Jessica Klebe <j.s.klebe@gmx.de>
2021-05-16 23:06:35 +0200

    added postprocessing

---
+++ Segmentation/postprocessing.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def post_process(img):
+    size = 11
+    dc = dominant_class(img)
+    img_bin = np.where(img == dc, 1, 0)
+    img_bin = close(img_bin, size)
+    img = np.where(img_bin == 1, dc, img)
+
+    
+    img_bin = np.where(img == 7, 1, 0)
+    img_bin = close(img_bin, size)
+    img = np.where(img_bin == 1, 7, img)
+    
+    img_bin = np.where(img == 7, 1, 0)
+    img_bin = close(img_bin, size)
+    img = np.where(img_bin == 1, 7, img)
+    
+    img_bin = np.where(img == dc, 1, 0)
+    img_bin = close(img_bin, size)
+    img = np.where(img_bin == 1, dc, img)
+    
+    #img_bin = close(img_bin, 9)
+
+    
+    return img
+
+def close(img, size = 7):
+    out = np.zeros((1, img.shape[1] + size - 1, img.shape[2] + size - 1))
+
+    for i in range(size):
+        for j in range(size):
+            if (i - size // 2)**2 + (j -size // 2) ** 2 > (size // 2) ** 2:
+                continue
+            out = out + np.pad(img, ((0,0), (i, size - i - 1), (j, size - j - 1)))
+    out = np.where(out > 0, 1, 0)
+    out = out[:, size // 2:-size // 2 + 1, size // 2:-size // 2 + 1]
+    return out
+
+def dominant_class(img):
+    a = np.zeros(7)
+    for i in range(7):
+        a[i] = np.sum(np.where(img == i, 1, 0))
+    print(np.argmax(a))
+
+    return np.argmax(a)
